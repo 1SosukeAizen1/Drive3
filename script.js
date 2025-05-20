5c1c24f0ec751ad6e4d237e15ae2fb8d0476f298
1SosukeAizen1 <aizengoku1@gmail.com>
2025-05-20 02:21:55 -0500

    yes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Hardcoded default screen types
-var screenType = "drive3"; // Main menu
+var screenType = "drive2"; // Main menu
 var secondaryScreenType = "drive3/2"; // Secondary menu
 
 // Function to load the slideshow
