7172aac280943d1296e233e47b9fe15856a97104
1SosukeAizen1 <aizengoku1@gmail.com>
2026-06-27 10:21:38 -0500

    new shakes

---
+++ script.js
@@ -0,0 +1,22 @@
+var backgrounds = [
+    'assets/background/Bowls.jpg',
+    'assets/background/shakes.png'
+];
+
+var layers = [
+    document.getElementById('bg-layer-1'),
+    document.getElementById('bg-layer-2')
+];
+
+layers[0].style.backgroundImage = "url('" + backgrounds[0] + "')";
+layers[1].style.backgroundImage = "url('" + backgrounds[1] + "')";
+
+var currentLayer = 0;
+
+function switchBackground() {
+    layers[currentLayer].classList.remove('active');
+    currentLayer = 1 - currentLayer;
+    layers[currentLayer].classList.add('active');
+}
+
+setInterval(switchBackground, 15000);
